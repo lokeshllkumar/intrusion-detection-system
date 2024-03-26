89f3f25713fae609ce60d0651b8374a4e3d55e7b
HarishSD2405 <harishsd2405@gmail.com>
2024-03-26 22:50:14 +0530

    created streamlit app

--- deploy.py
+++ deploy.py
@@ -15,3 +15,19 @@ with open("models\gnb_pickle.pkl", "rb") as file:
 
 with open("models\gradient_tree_pickle.pkl", "rb") as file:
     gradient_tree_pickle = pk.load(file)
+
+src_bytes = st.number_input("src_bytes")
+dst_bytes = st.number_input("dest_bytes")
+same_srv_rate = st.number_input("same_srv_rate")
+flag = st.number_input("flag")
+diff_srv_rate = st.number_input("diff_srv_rate")
+dst_host_srv_count = st.number_input("dst_host_srv_count")
+dst_host_same_srv_rate = st.number_input("dst_host_same_srv_rate")
+count = st.number_input("count")
+protocol_type = st.number_input("protocol_type")
+serror_rate = st.number_input("serror_rate")
+
+predicted_class = gnb_pickle.predict(np.array([[src_bytes, dst_bytes, same_srv_rate, flag, diff_srv_rate, dst_host_srv_count, dst_host_same_srv_rate, count, protocol_type, serror_rate]]))
+
+if st.button("Predict"):
+    st.write(f"Predicted class: {predicted_class}")
